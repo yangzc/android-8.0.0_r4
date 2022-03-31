41b066556654ba6f860d5529fd8ebe563a485892
Your Name <zhc_yang@163.com>
2022-03-31 17:36:28 +0800

    ss

--- bionic/libc/kernel/uapi/linux/netfilter/xt_tcpmss.h
+++ bionic/libc/kernel/uapi/linux/netfilter/xt_tcpmss.h
@@ -16,13 +16,13 @@
  ***
  ****************************************************************************
  ****************************************************************************/
-#ifndef _XT_TCPMSS_MATCH_H
-#define _XT_TCPMSS_MATCH_H
+#ifndef _XT_TCPMSS_H
+#define _XT_TCPMSS_H
 #include <linux/types.h>
-struct xt_tcpmss_match_info {
+struct xt_tcpmss_info {
 /* WARNING: DO NOT EDIT, AUTO-GENERATED CODE - SEE TOP FOR INSTRUCTIONS */
-  __u16 mss_min, mss_max;
-  __u8 invert;
+  __u16 mss;
 };
+#define XT_TCPMSS_CLAMP_PMTU 0xffff
 #endif
 /* WARNING: DO NOT EDIT, AUTO-GENERATED CODE - SEE TOP FOR INSTRUCTIONS */
